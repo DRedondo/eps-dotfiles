eafe9c223af73be5d4712a2e56c1440a097964f3
Daniel Redondo <daniel.redondoc@estudiante.uam.es>
2016-02-02 12:55:56 +0100

    Pruebas

--- git-user.sh
+++ git-user.sh
@@ -2,7 +2,7 @@
 ## git-user.sh -- Descomenta las siguientes lineas y copialo a custom/
 ##
 
-#export GIT_AUTHOR_NAME="John Smith"
-#export GIT_AUTHOR_EMAIL="johnsmith@example.com"
+#export GIT_AUTHOR_NAME="John"
+#export GIT_AUTHOR_EMAIL="john@example.com"
 #export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 #export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
